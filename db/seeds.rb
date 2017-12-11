42a46f608a070f20fb085b4d01efd93fc74aa902
tylermiller94 <tylermiller94@gmail.com>
2017-12-11 07:22:00 -0800

    seed admin/user accounts, Integrate admin_rails gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  User.destroy_all
+
+  def self.begin
+    seed = Seed.new
+
+    User.create!(email: "admin@admin.com", password: "password", admin: true)
+    User.create!(email: "user@user.com",password: "password")
+  end
+
+
+Seed.begin
