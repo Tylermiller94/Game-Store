6c355bd203172252e79690db50db0ca92c8140dd
tylermiller94 <tylermiller94@gmail.com>
2017-12-10 21:45:54 -0800

    Add New and Create actions with views files

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,17 @@ class GamesController < ApplicationController
 
   def index
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :developer)
 end
